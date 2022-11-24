c4487f9a026e1d90f786168172caffaee4ffd0f4
Gerrit Giliomee <giliomeejg@gmail.com>
2022-11-24 10:29:08 +0200

    func(Crawler): Pause every now and then to not overload the web server.

--- go.py
+++ go.py
@@ -1,11 +1,10 @@
-
-
 import os
 import requests
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from saveFullHtmlPage import saveFullHtmlPage
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
@@ -24,15 +23,19 @@ for sitemap in sitemaps:
     loc_tags = parcala.find_all('loc')
     kwargs = {'bypass_robots': True}
 
+    counter = -1
     for loc in loc_tags:
+        counter += 1
         urls_from_xml.append(loc.get_text()) 
         # page = loc.get_text().rsplit('/', 1)[-1]
 
         path = urlparse(loc.get_text()).path
         
-        print('page: ' + path)
+        print('Page: ' + path)
         
         os.makedirs(outputDir + path, exist_ok=True)
         saveFullHtmlPage(loc.get_text(), outputDir + path)
 
-
+        if counter > int(os.getenv('CRAWLING_PAUSE_EVERY_NTH_REQUEST')) and (counter % int(os.getenv('CRAWLING_PAUSE_EVERY_NTH_REQUEST'))) == 0:
+            print('Pausing for ' + os.getenv('CRAWLING_PAUSE_IN_SECONDS') + ' seconds')
+            time.sleep(int(os.getenv('CRAWLING_PAUSE_IN_SECONDS')))
